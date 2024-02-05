095a37917675239b6cc13d584ebe97fd1862be6a
MTtankkeo <ttankkeo112@gamil.com>
2024-02-06 04:29:07 +0900

    Add relValue

--- test/js/main.js
+++ test/js/main.js
@@ -1,7 +1,6 @@
-import { AnimationStatus } from "../../js/animation_controller.js";
+import { AnimationController, AnimationStatus } from "../../js/animation_controller.js";
 import { Color } from "../../js/color.js";
 import { ColorTween } from "../../js/color_tween.js";
-import { Curve } from "../../js/cubic.js";
 import { CurvedAnimation } from "../../js/curved_animation.js";
 
 
@@ -10,7 +9,7 @@ const box = document.getElementById("box");
 const percentText = document.getElementById("percent_text");
 const button = document.getElementById("animate");
 
-const controller = new CurvedAnimation(500, Curve.Ease);
+const controller = new CurvedAnimation(500);
 
 const colorTween = new ColorTween(Color.var("--red"), Color.var("--blue"));
 
@@ -18,7 +17,7 @@ controller.addListener(value => {
     const parent = box.parentElement.getBoundingClientRect();
     const ract = box.getBoundingClientRect();
     
-    box.style.transform = `translate(${(parent.width - ract.width)   * value}px, 0px)`;
+    box.style.transform = `translate(${(parent.width - ract.width) * value}px, 0px)`;
     box.style.backgroundColor = colorTween.transform(value).toHex();
     percentText.textContent = `${Math.round(value * 100)}%`;
 });
