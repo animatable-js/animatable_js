b53ea6bae62dcf777c627c27c8d27ae0dda0ba32
MTtankkeo <ttankkeo112@gmail.com>
2024-03-23 07:01:36 +0900

    dist

---
+++ dist/types/type.d.ts
@@ -0,0 +1,5 @@
+import { AnimationStatus } from "./animatable";
+/** A elapsed duration between the previous frame and the current frame is given. */
+export type TickerCallback = (elapsedDelta: number) => void;
+export type AnimationListener = (value: number) => void;
+export type AnimationStatusListener = (status: AnimationStatus) => void;
