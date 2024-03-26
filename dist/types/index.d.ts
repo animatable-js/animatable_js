d000b196539a9b98cf1ed0f4698726f70e8849e5
MTtankkeo <ttankkeo112@gmail.com>
2024-03-26 13:13:55 +0900

    Update README.md and small-fix

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,6 +1,7 @@
 export { Ticker } from "./ticker";
 export { Animatable, AnimationStatus } from "./animatable";
 export { Animation } from "./animation";
+export { AnimationListenable } from "./animation_listenable";
 export { AnimationController } from "./animation_controller";
 export { Tween, NumberTween, ColorTween } from "./tween";
 export { Cubic, CubicPoint, Curve } from "./cubic";
