f7fbb23b5a791bb5ff2c9f4cf50f428a6e0ba4c2
MTtankkeo <ttankkeo112@gmail.com>
2024-05-24 16:36:25 +0900

    Add global-variable-name

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,15 @@ import typescript from "rollup-plugin-typescript2";
 import { terser } from "rollup-plugin-terser";
 import extensions from "rollup-plugin-extensions"
 
+// The browser global variable name to refer to this package.
+const globals = {
+    "animatable-js": "AnimJS",
+}
+
 export default {
     input: "src/index.ts",
     output: [
-        { file: "dist/index.js", format: "esm", sourcemap: true, name: "AnimJS" },
+        { file: "dist/index.js", format: "esm", sourcemap: true, globals: globals },
     ],
     plugins: [
         extensions({
